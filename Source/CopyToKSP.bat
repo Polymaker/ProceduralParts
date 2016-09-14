e22ca3d0be8cc8c28948f5a8d75aed106ae0ede8
James Turner <james_w_turner27@hotmail.com>
2016-09-14 11:30:19 -0400

    actually added FilterEditor MM patch (wasn't staged in last commit)

---
+++ Source/CopyToKSP.bat
@@ -0,0 +1,22 @@
+REM set kspPPPath=D:\Steam\SteamApps\common\Kerbal Space Program\GameData\ProceduralParts\
+set kspPPPath=D:\Games\Steam\steamapps\common\Kerbal Space Program\GameData\ProceduralParts\
+set buildDir=%1
+
+cd %~dp0
+pushd %~dp0
+
+IF [%1]==[] (
+	set buildDir="bin\Test KSP\"
+)
+
+xcopy %buildDir%*.dll "..\Plugins\" /C /D /Y /I
+
+cd ..
+echo Copying Plugins dlls...
+xcopy Plugins "%kspPPPath%Plugins\" /s /e /y /d
+echo Copying Parts configs...
+xcopy Parts "%kspPPPath%Parts\" /s /e /y /d /i
+echo Copying ModuleManager patches...
+xcopy ModuleManager "%kspPPPath%ModuleManager\" /s /e /y /d /i
+
+popd
